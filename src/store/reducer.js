29599b0d81d3c620de7081df02998a98134ffc88
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-03-27 20:22:55 +0530

    Initial commit

---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    length: 10,
+    options: {
+        symbols: true,
+        numbers: true
+    },
+    password: 'password'
+};
+
+const reducer = (state = initialState, action) {
+    return state;
+}
+
+export default reducer;
