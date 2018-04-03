01431c434ed07b7d74dbea56ddbc90425850e814
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-03 14:25:08 +0530

    Setup redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from './store/reducer';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
 registerServiceWorker();
