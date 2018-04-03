7176091f950b493cdcfc3ffc943f4358b0b763d1
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-03 15:37:39 +0530

    Connect redux to slider

--- src/components/LengthSlider/LengthSlider.js
+++ src/components/LengthSlider/LengthSlider.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
-const LengthSlider = (props) => {
-    return (
-        <section className="section">
-            <div className="container">
-                <div className="tile is-parent has-text-centered">
-                    <div className="tile is-child">
-                        <p className="subtitle">Slider will go here</p>
+class LengthSlider extends Component {
+    render() {
+        return (
+            <section className="section" >
+                <div className="container">
+                    <div className="tile is-parent has-text-centered">
+                        <div className="tile is-child">
+                            <p className="subtitle">Slider will go here</p>
+                            <button onClick={this.props.onLengthChange}>Change</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }
 
-export default LengthSlider;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLengthChange: () => dispatch({ type: actionTypes.CHANGE_PASSWORD_LENGTH })
+    }
+};
+
+export default connect(null, mapDispatchToProps)(LengthSlider);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,21 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.CHANGE_PASSWORD_LENGTH:
+            const newLength = Math.random();
+            return {
+                ...state,
+                length: newLength
+            }
+        case actionTypes.GENERATE_PASSWORD:
+            const newPassword = Math.random();
+            return {
+                ...state,
+                password: newPassword
+            }
+    }
+
     return state;
 }
 
