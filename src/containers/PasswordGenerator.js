1cfc996e7718995ffc1b83057fb0d8e99d7fc81f
Narendra Vaghela <narendravaghela4389@gmail.com>
2018-04-04 17:29:05 +0530

    Convert to functional component

--- src/containers/PasswordGenerator.js
+++ src/containers/PasswordGenerator.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PasswordLength from '../components/PasswordLength/PasswordLength';
 import PasswordOptions from '../components/PasswordOptions/PasswordOptions';
 import Password from '../components/Password/Password';
 import LengthSlider from '../components/LengthSlider/LengthSlider';
 
-class PasswordGenerator extends Component {
-    render() {
-        return (
-            <div>
-                <PasswordLength />
-                <LengthSlider />
-                <section class="section password-options-container">
-                    <div className="columns">
-                        <div className="column is-half">
-                            <PasswordOptions />
-                        </div>
-                        <div className="column is-half">
-                            <Password />
-                        </div>
+const PasswordGenerator = (props) => {
+    return (
+        <div>
+            <PasswordLength />
+            <LengthSlider />
+            <section class="section password-options-container">
+                <div className="columns">
+                    <div className="column is-half">
+                        <PasswordOptions />
                     </div>
-                </section>
-            </div>
-        );
-    }
+                    <div className="column is-half">
+                        <Password />
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default PasswordGenerator;
